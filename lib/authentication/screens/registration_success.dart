3a866112a7ac3749cbeece75d1a156c83f92c0db
FlutterBugs <aloaderemi1@gmail.com>
2023-05-10 20:07:10 +0100

    added choose_goal page

---
+++ lib/authentication/screens/registration_success.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:fitnest/constants.dart';
+import 'package:get/get.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class RegistrationSuccess extends StatelessWidget {
+  const RegistrationSuccess({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
