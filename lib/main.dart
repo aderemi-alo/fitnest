8bd37707d9062e1d301d97f47a6dfc678e8e5f70
FlutterBugs <aloaderemi1@gmail.com>
2023-05-11 13:07:50 +0100

    Linked app with firebase project

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,12 @@ import 'package:flutter/material.dart';
 import 'onboarding/screens/welcome_screen.dart';
 import 'package:get/get.dart';
 import 'constants.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
 
